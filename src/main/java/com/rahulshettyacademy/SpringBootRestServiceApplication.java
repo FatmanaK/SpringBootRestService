7ec2df5775dae5e3fb377cfc12b5dd638937f031
FatmanaK <janalikbolsin@gmail.com>
2024-05-02 14:27:05 -0500

    Udemy Rahul spring

--- src/main/java/com/rahulshettyacademy/SpringBootRestServiceApplication.java
+++ src/main/java/com/rahulshettyacademy/SpringBootRestServiceApplication.java
@@ -9,5 +9,5 @@ public class SpringBootRestServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRestServiceApplication.class, args);
 	}
-
+  //start for spring
 }
