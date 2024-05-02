abcaa1c6d44c0ebe83191b1e9aff5eaf80fffdd7
FatmanaK <janalikbolsin@gmail.com>
2024-05-02 14:24:14 -0500

    Udemy Rahul spring

---
+++ src/test/java/com/rahulshettyacademy/SpringBootRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahulshettyacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
