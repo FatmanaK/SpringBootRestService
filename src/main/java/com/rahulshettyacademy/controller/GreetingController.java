19ab12a5632369032abd4569b463cb3592bde221
FatmanaK <janalikbolsin@gmail.com>
2024-05-03 11:35:13 -0500

    add greetingcontroller class

---
+++ src/main/java/com/rahulshettyacademy/controller/GreetingController.java
@@ -0,0 +1,14 @@
+package com.rahulshettyacademy.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+    @GetMapping("/greeting")
+    public void greeting (@RequestParam(value="name")String name){
+
+    }
+
+}
