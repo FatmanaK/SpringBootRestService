10f7e3899687794290e7445ec49858751a863cbb
FatmanaK <janalikbolsin@gmail.com>
2024-05-05 12:43:46 -0500

    changing on Library

--- src/main/java/com/learningspring/controller/LibraryController.java
+++ src/main/java/com/learningspring/controller/LibraryController.java
@@ -3,6 +3,7 @@ package com.learningspring.controller;
 
 import com.learningspring.repository.LibraryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,12 +20,17 @@ public class LibraryController {
     public ResponseEntity addBookImplement (@RequestBody Library library){
 
            AddResponse ad = new AddResponse();
-           library.setId(library.getIsbn()+library.getAisle());
+           String id=library.getIsbn()+library.getAisle();
+           library.setId(id);
+          // library.setId(library.getIsbn()+library.getAisle());
            repository.save(library);
+
+        HttpHeaders headers=new HttpHeaders();
+        headers.add("unique",id);
            ad.setMsg("Success book is added");
            ad.setId(library.getIsbn()+library.getAisle());
           // return ad;
-        return new ResponseEntity<AddResponse>(ad, HttpStatus.CREATED);
+        return new ResponseEntity<AddResponse>(ad,headers, HttpStatus.CREATED);
 
 
 
--- src/main/java/com/learningspring/repository/LibraryRepository.java
+++ src/main/java/com/learningspring/repository/LibraryRepository.java
@@ -2,9 +2,8 @@ package com.learningspring.repository;
 
 import com.learningspring.controller.Library;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface LibraryRepository extends JpaRepository<Library, String>{
 
 }
