bb4dc7b02a9c9e9945128abe60e3e0953605951e
FatmanaK <janalikbolsin@gmail.com>
2024-05-22 11:33:30 -0500

    add data.sql

--- src/main/java/com/learningspring/controller/LibraryController.java
+++ src/main/java/com/learningspring/controller/LibraryController.java
@@ -3,6 +3,7 @@ package com.learningspring.controller;
 
 import com.learningspring.repository.LibraryRepository;
 import com.learningspring.service.LibraryService;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -12,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-import java.util.logging.Logger;
 
 @RestController
 public class LibraryController {
